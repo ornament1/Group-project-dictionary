5b5464582636f6a0f35e7035a776e84a940bc9fc
ornament1 <aanwulidiunor@gmail.com>
2025-01-04 12:05:10 +0100

    Update tkinter_language_dictionary.py

--- tkinter_language_dictionary.py
+++ tkinter_language_dictionary.py
@@ -9,6 +9,20 @@ languages = {
     "por favor": "please",    # Spanish
     "gracias": "thank you",   # Spanish
     "sí": "yes",              # Spanish
-    "no": "no",            #Spanish
+    "no": "no",               #Spanish
+    "buenos días": "good morning",     #Spanish
+    "buenas tardes": "good afternoon",  #Spanish
+    "buenas noches": "good night",       #Spanish
+    "¿cómo estás?": "how are you?",     #Spanish
+    "bien": "well",         #Spanish
+    "mal": "bad",        #Spanish
+    "¿qué tal?": "what's up?",     #Spanish
+    "amigo": "friend",      #Spanish
+    "familia": "family",     #Spanish
+    "casa": "house",       #Spanish
+    "comida": "food",         #Spanish
+    "agua": "water",       #Spanish
+    "libro": "book",         #Spanish
+    "escuela": "school",      #Spanish
     },
 }
