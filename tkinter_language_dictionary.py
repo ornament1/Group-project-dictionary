790c5385cd7bf236400cfdaee85c7b6a25e0a009
ornament1 <aanwulidiunor@gmail.com>
2025-01-04 11:59:16 +0100

    initial commit

---
+++ tkinter_language_dictionary.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+# Dictionary of languages and their translations
+languages = {
+    "Spanish": {
+    "hola": "hello",          # Spanish
+    "adiós": "goodbye",       # Spanish
+    "por favor": "please",    # Spanish
+    "gracias": "thank you",   # Spanish
+    "sí": "yes",              # Spanish
+    "no": "no",            #Spanish
+    },
+}
